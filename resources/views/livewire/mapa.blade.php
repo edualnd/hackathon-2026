a28c3ca5d5160d823e5432c0fac455614292e447
MrllMoreira <mrll.moreira01@gmail.com>
2026-07-04 23:16:02 -0300

    add: localizar usuario

--- resources/views/livewire/mapa.blade.php
+++ resources/views/livewire/mapa.blade.php
@@ -1,20 +1,77 @@
 <x-guest-layout>
 
 <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css">
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
-    <div id="map" style="height: 500px"></div>
+<div id="map" style="height:500px"></div>
+
 <div id="card-escola" class="mt-4 p-4 border rounded shadow">
     <p>Clique em uma escola no mapa.</p>
 </div>
-    <script>
-    const map = L.map('map').setView([-23.6205, -45.4132], 12);
-    const escolas = @json($escolas);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap'
+
+<script>
+
+const escolas = @json($escolas);
+
+const map = L.map('map').setView([-23.6205, -45.4132], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+const card = document.getElementById('card-escola');
+
+
+//
+// NOVA FUNCIONALIDADE
+//
+
+let marcadorUsuario;
+let circuloPrecisao;
+
+// solicita a localização
+map.locate({
+    setView: true,
+    maxZoom: 15,
+    enableHighAccuracy: true
+});
+
+// localização encontrada
+map.on('locationfound', function(e){
+
+    // remove marcador anterior caso exista
+    if(marcadorUsuario){
+        map.removeLayer(marcadorUsuario);
+    }
+
+    if(circuloPrecisao){
+        map.removeLayer(circuloPrecisao);
+    }
+
+    marcadorUsuario = L.marker(e.latlng)
+        .addTo(map)
+        .bindPopup("<strong>Você está aqui</strong>")
+        .openPopup();
+
+    circuloPrecisao = L.circle(e.latlng,{
+        radius: e.accuracy
     }).addTo(map);
-    
-    const card = document.getElementById('card-escola');
+
+});
+
+// erro ao obter localização
+map.on('locationerror', function(e){
+
+    console.error(e.message);
+
+    alert("Não foi possível obter sua localização.");
+
+});
+
+
+//
+// ESCOLAS (permanece igual)
+//
 
 escolas.forEach(escola => {
 
@@ -24,9 +81,11 @@ escolas.forEach(escola => {
     ]).addTo(map);
 
     marker.on('click', () => {
-const vagas = escola.vagas
-    .map(vaga => `<li>${vaga.serie}: ${vaga.qtd}</li>`)
-    .join('');
+
+        const vagas = escola.vagas
+            .map(vaga => `<li>${vaga.serie}: ${vaga.qtd}</li>`)
+            .join('');
+
         card.innerHTML = `
             <h2>${escola.nome}</h2>
 
@@ -39,13 +98,16 @@ const vagas = escola.vagas
             <p><strong>Integral:</strong> ${escola.integral ? 'Sim' : 'Não'}</p>
 
             <h3>Vagas</h3>
-    <ul>
-        ${vagas}
-    </ul>
-        `;
-})
-    });
-    </script>
 
-  
+            <ul>
+                ${vagas}
+            </ul>
+        `;
+
+    });
+
+});
+
+</script>
+
 </x-guest-layout>
